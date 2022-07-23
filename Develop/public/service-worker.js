4f8397062fe5d54b8d4c5a395b9effaee608eb49
Trevor Snell <trevorsnell@optonline.net>
2022-07-23 16:23:29 -0400

    update

---
+++ Develop/public/service-worker.js
@@ -0,0 +1,59 @@
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE = APP_PREFIX + VERSION;
+
+const FILE_CACHE = [
+    './index.html',
+    './css/styles.css',
+    './js/index.js',
+    './js/idb.js',
+    './manifest.json',
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x152.png',
+    './icons/icon-192x192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512x512.png',
+];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE).then(function (cache) {
+            return cache.addAll(FILE_CACHE)
+        })
+    )
+});
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheList.push(CACHE);
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheList.indexOf(key)=== -1) {
+                    return caches.delete(keyList[i]);
+                }
+            }));
+        })
+    )
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url);
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url);
+                return fetch(e.request)
+            }
+
+        })
+    )
+});
